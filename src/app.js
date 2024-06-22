628733ff3fd63375651efc092d1ad3a1cfa951d5
pranavpatel395 <pranavpatel828@gmail.com>
2024-06-22 08:46:34 +0530

    user and video model

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import cookieParser from "cookie-parser"
+import express from "express"
+import cookieParser from "cookie-parser"
+const app = express()
+
+
+app.use(cors())
+app.use (express.urlencoded({extended:true}))
+app.use(express.json())
+app.use(express.static("public"))
+app.use(cookieParser())
+
+export default app
